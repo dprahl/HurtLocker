cee889dfb52cdc9559d8329e6fafa5f32a4cd422
Daniel Prahl <daniel.prahl@gmail.com>
2017-05-31 19:32:55 -0400

    "added ShoppingListEntry class"

--- src/main/java/ShoppingListEntry.java
+++ src/main/java/ShoppingListEntry.java
@@ -1,10 +1,53 @@
-import java.util.ArrayList;
-
 /**
  * Created by danielprahl on 5/31/17.
  */
 public class ShoppingListEntry {
-    private String item;
-    private ArrayList<Double> prices;
+    private String name;
     private int count;
+    private double firstPrice;
+    private int firstPriceCount;
+    private double secondPrice;
+    private int secondPriceCount;
+
+    public ShoppingListEntry(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getItemCount() {
+        return count;
+    }
+
+    public void addOneToCount(){
+        this.count++;
+    }
+
+    public double getFirstPrice() {
+        return firstPrice;
+    }
+
+    public int getFirstPriceCount() {
+        return firstPriceCount;
+    }
+
+    public double getSecondPrice() {
+        return secondPrice;
+    }
+
+    public int getSecondPriceCount() {
+        return secondPriceCount;
+    }
+
+    public void addPrice(double price){
+        if(this.firstPrice == 0){
+            this.firstPrice = price;
+            this.firstPriceCount++;
+        }else if(this.secondPrice == 0){
+            this.secondPrice = price;
+            this.secondPriceCount++;
+        }
+    }
 }
--- src/main/java/ShoppingListHandler.java
+++ src/main/java/ShoppingListHandler.java
@@ -1,6 +1,10 @@
+import java.util.ArrayList;
+
 /**
  * Created by danielprahl on 5/31/17.
  */
 public class ShoppingListHandler {
+    ArrayList<InventoryItem> items;
+
 
 }
